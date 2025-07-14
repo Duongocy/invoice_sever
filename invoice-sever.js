b636756515c93af683d6dcc5d5bce4bdc19cd6a6
Y Duong Ngoc <ydn@hoya.com>
2025-07-14 14:10:36 +0700

    Thong tin user 7

--- invoice-sever.js
+++ invoice-sever.js
@@ -17,22 +17,18 @@ const pool = new Pool({
 
 // Lấy dữ liệu từ bảng invoice_table
 app.get('/Invoice', async (req, res) => {
-    console.log("Đã nhận được yêu cầu từ client - ip công khai");//báo trên log là đã nhận được 1 yêu cầu từ client
     let kieu_yeu_cau = req.query.yeucau;//Lấy giá trị thuột tính type của request từ client gán cho biến type
-    console.log("Yêu cầu = ",kieu_yeu_cau);
-    let userid = req.query.userid; //Lấy ID của user
-    console.log("ID = ",userid);
+    let user_id = req.query.userid;
     let so_id = req.query.invid;//lấy số ID của invoice
+    console.log("Đã nhận được yêu cầu từ client - ip công khai");//báo trên log là đã nhận được 1 yêu cầu từ client
     if (kieu_yeu_cau ==='layhoadon') {
-        try {
-            
-            query_string = "SELECT invoice_id,invoice_title,customer, SUM(price*quantity) AS amount,invoice_date FROM invoice_table WHERE user_id ='"+userid+"' GROUP BY invoice_id,invoice_date,invoice_title,customer ORDER BY invoice_date DESC;";
-            console.log(query_string);
+        try {            
+            query_string = "SELECT invoice_id,invoice_title,customer, SUM(price*quantity) AS amount,invoice_date FROM invoice_table WHERE user_id ='"+user_id+"' GROUP BY invoice_id,invoice_date,invoice_title,customer ORDER BY invoice_date DESC;"
             const result = await pool.query(query_string);
-            res.status(201).json({ message: 'Lấy danh sách hóa đơn thành công' });
+            res.json(result.rows);
         } catch (err) {
             console.error(err);
-            res.status(500).json({ message: 'Không lấy được danh sách hóa đơn' });
+            res.status(500).send('Lỗi khi lấy dữ liệu');
         }
     }
     else if(kieu_yeu_cau === 'chitiethoadon'){
@@ -43,7 +39,7 @@ app.get('/Invoice', async (req, res) => {
             res.json(result.rows);
         } catch (err) {
             console.error(err);
-            res.status(500).json({ message: 'Không lấy được chi tiết hóa đơn' });
+            res.status(500).send('Lỗi khi lấy dữ liệu');
         }
     }
 });
@@ -56,10 +52,10 @@ app.post('/Invoice', async (req, res) => {
     try {
         results = [];
             for (product of product_array){
-                const {user_id, user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity} = product;
+                const {invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity} = product;
                 const result = await pool.query(
-                'INSERT INTO invoice_table (user_id,user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9) RETURNING *',
-                    [user_id,user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity]
+                'INSERT INTO invoice_table (invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity) VALUES ($1, $2, $3,$4,$5,$6,$7) RETURNING *',
+                    [invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity]
                 );
                 results.push(result.rows[0]); // Lưu kết quả vào mảng
             }
