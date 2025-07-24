4c0a664d562962637134686720b53260c0f40f8c
Dương Ngọc Ý <duongocy@gmail.com>
2025-07-25 06:11:13 +0700

    Thay doi dia chi ip cong khai 1

--- invoice-sever.js
+++ invoice-sever.js
@@ -10,7 +10,7 @@ app.use(express.json());
 // Cấu hình kết nối đến PostgreSQL
 const pool = new Pool({
     user: 'postgres', // Thay thế bằng username của bạn
-    host: '14.165.172.59',//địa chỉ ip công khai của máy fujitsu 
+    host: '123.19.121.187',//địa chỉ ip công khai của máy fujitsu 
     database: 'Invoice',
     password: '1!Ngaycuoicung', // Thay thế bằng password của bạn
     port: 5432,
@@ -22,7 +22,7 @@ app.get('/Invoice', async (req, res) => {
     const token = authHeader?.split(' ')[1];
 
     if (!token) return res.sendStatus(401); //nếu không có token thì thoát luôn không thực hiện đoạn sau 
-    tokenlib.verify(token,'DuoNgocY', (err, user) => {//xác thực token
+    tokenlib.verify(token, 'DuoNgocY', (err, user) => {//xác thực token
         if (err) return res.sendStatus(403); //nếu token bị lỗi hoặc hết hạn cũng thoát luôn không thực hiện đoạn dưới
 
     });
@@ -31,12 +31,12 @@ app.get('/Invoice', async (req, res) => {
     let user_id = req.query.userid;
     let so_id = req.query.invid;//lấy số ID của invoice
     console.log("Đã nhận được yêu cầu từ client - ip công khai");//báo trên log là đã nhận được 1 yêu cầu từ client
-    console.log("Kiểu yêu cầu : ",kieu_yeu_cau);
-    console.log("User ID : ",user_id);
-    if (kieu_yeu_cau ==='layhoadon') {
-        try {            
-            query_string = "SELECT invoice_id,invoice_title,customer, SUM(price*quantity) AS amount,invoice_date FROM invoice_table WHERE user_id ='"+user_id+"' GROUP BY invoice_id,invoice_date,invoice_title,customer ORDER BY invoice_date DESC;"
-            console.log("Câu truy vấn : ",query_string);
+    console.log("Kiểu yêu cầu : ", kieu_yeu_cau);
+    console.log("User ID : ", user_id);
+    if (kieu_yeu_cau === 'layhoadon') {
+        try {
+            query_string = "SELECT invoice_id,invoice_title,customer, SUM(price*quantity) AS amount,invoice_date FROM invoice_table WHERE user_id ='" + user_id + "' GROUP BY invoice_id,invoice_date,invoice_title,customer ORDER BY invoice_date DESC;"
+            console.log("Câu truy vấn : ", query_string);
             const result = await pool.query(query_string);
             res.json(result.rows);
         } catch (err) {
@@ -44,9 +44,9 @@ app.get('/Invoice', async (req, res) => {
             res.status(500).send('Lỗi khi lấy dữ liệu');
         }
     }
-    else if(kieu_yeu_cau === 'chitiethoadon'){
+    else if (kieu_yeu_cau === 'chitiethoadon') {
         try {
-            query_string = "SELECT product_name,quantity,price, SUM(price*quantity) AS amount FROM invoice_table WHERE invoice_id='" +so_id + "' group by product_name,quantity,price;"; 
+            query_string = "SELECT product_name,quantity,price, SUM(price*quantity) AS amount FROM invoice_table WHERE invoice_id='" + so_id + "' group by product_name,quantity,price;";
             console.log(query_string);
             const result = await pool.query(query_string);
             res.json(result.rows);
@@ -65,16 +65,16 @@ app.post('/Invoice', async (req, res) => {
     // console.log(product_name,price,quantity);
     try {
         results = [];
-            for (product of product_array){
-                const {user_id,user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity} = product;
-                const result = await pool.query(
+        for (product of product_array) {
+            const { user_id, user_name, invoice_id, invoice_title, invoice_date, customer, product_name, price, quantity } = product;
+            const result = await pool.query(
                 'INSERT INTO invoice_table (user_id, user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity) VALUES ($1, $2, $3,$4,$5,$6,$7,$8,$9) RETURNING *',
-                    [user_id,user_name,invoice_id, invoice_title,invoice_date,customer,product_name,price, quantity]
-                );
-                results.push(result.rows[0]); // Lưu kết quả vào mảng
-            }
-            res.status(201).json(results);//không gởi phản hồi trong vòng for vì nó sẽ kết thúc việc lưu dữ liệu ngay sau vòng lặp đầu tiên
+                [user_id, user_name, invoice_id, invoice_title, invoice_date, customer, product_name, price, quantity]
+            );
+            results.push(result.rows[0]); // Lưu kết quả vào mảng
         }
+        res.status(201).json(results);//không gởi phản hồi trong vòng for vì nó sẽ kết thúc việc lưu dữ liệu ngay sau vòng lặp đầu tiên
+    }
     catch (err) {
         console.error(err);
         res.status(500).send('Lỗi khi thêm dữ liệu');
@@ -83,8 +83,8 @@ app.post('/Invoice', async (req, res) => {
 
 //1 luồng đơn giản để nhận tín hiệu ping từ trang web cron job (https://console.cron-job.org/jobs)giữ api trên render luôn thức
 app.get('/ping', (req, res) => {
-  res.send('pong!');
-  console.log('Vừa nhận tín hiệu Ping từ cron-job!');
+    res.send('pong!');
+    console.log('Vừa nhận tín hiệu Ping từ cron-job!');
 });
 
 // Khởi động server
